56d68c2d5fe686e5a2f8847ea9195e1a05887963
Scalorn Scorpus <scalorn@gmail.com>
2026-05-11 16:37:08 -0700

    feat: Create OE2EmpireTracker.Common (.NET Standard 2.0) shared library
    - Created OE2EmpireTracker.Common project targeting .NET Standard 2.0
    
    - Added to solution

---
+++ OE2EmpireTracker.Common/CommonInfo.cs
@@ -0,0 +1,12 @@
+namespace OE2EmpireTracker.Common
+{
+    /// <summary>
+    /// Placeholder to verify the Common library builds and can be referenced.
+    /// This will be replaced with actual shared models and services.
+    /// </summary>
+    public static class CommonInfo
+    {
+        /// <summary>Gets the Common library version.</summary>
+        public static string Version => "0.1.0";
+    }
+}
