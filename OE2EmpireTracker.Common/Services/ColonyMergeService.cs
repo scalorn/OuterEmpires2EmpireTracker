8cc1165e08d3cee00e3d1692aa7126965f4cbc72
Scalorn Scorpus <scalorn@gmail.com>
2026-05-30 14:46:56 -0700

    Fix: assign DisplaySequence when creating structures from API
    New structures created by CreateStructureFromApi had DisplaySequence=0, showing as '#0' in the UI. Now assigns max(existing sequences) + 1 so new structures appear at the end of the list in proper order.
    
    Prompt: When it rescanned it added a reactor #0 and set it to building. Doesn't look like the correct sequence id is being set.

--- OE2EmpireTracker.Common/Services/ColonyMergeService.cs
+++ OE2EmpireTracker.Common/Services/ColonyMergeService.cs
@@ -119,14 +119,22 @@ namespace OE2EmpireTracker.Services
                 else
                 {
                     var newStructure = CreateStructureFromApi(apiBuilding);
+
+                    // Assign next available DisplaySequence
+                    int maxSeq = colony.Structures.Count > 0
+                        ? colony.Structures.Max(s => s.DisplaySequence)
+                        : 0;
+                    newStructure.DisplaySequence = maxSeq + 1;
+
                     colony.Structures.Add(newStructure);
                     hasChanges = true;
 
                     Log.Info(
-                        "MergeBuildings: created new structure UUID={0} BuildingID={1} Name='{2}' for colony {3}",
+                        "MergeBuildings: created new structure UUID={0} BuildingID={1} Name='{2}' seq={3} for colony {4}",
                         newStructure.UUID,
                         newStructure.BuildingID,
                         apiBuilding.BlueprintDesignName,
+                        newStructure.DisplaySequence,
                         colony.UUID);
                 }
             }
